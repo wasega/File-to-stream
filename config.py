d4f06d147d06f8983076c838f3f13eccf930861a
wasega <124245129+wasega@users.noreply.github.com>
2026-03-12 16:48:45 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,19 +6,19 @@ from dotenv import load_dotenv
 load_dotenv(".env")
 
 class Config:
-    API_ID = int(os.environ.get("API_ID", 0))
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    OWNER_ID = int(os.environ.get("OWNER_ID", 0))
+    API_ID = int(os.environ.get("29895586", 0))
+    API_HASH = os.environ.get("2e38dc8c9b16e0ae5890a6cae024b7d8", "")
+    BOT_TOKEN = os.environ.get("8611954505:AAGC5H_tomX01BnyUTWnTMrvZlGz2FSV9gY", "")
+    OWNER_ID = int(os.environ.get("1170382284", 0))
     
-    _storage_channel_str = os.environ.get("STORAGE_CHANNEL")
+    _storage_channel_str = os.environ.get("-1003290832112")
     if _storage_channel_str:
         try: STORAGE_CHANNEL = int(_storage_channel_str)
         except ValueError: STORAGE_CHANNEL = _storage_channel_str
     else: STORAGE_CHANNEL = 0
     
-    BASE_URL = os.environ.get("BASE_URL", "").rstrip('/')
-    DATABASE_URL = os.environ.get("DATABASE_URL", "")
+    BASE_URL = os.environ.get("https://file-to-stream-g4qi.onrender.com", "").rstrip('/')
+    DATABASE_URL = os.environ.get("mongodb+srv://waserecords:123bubu123@cluster0.vty48ho.mongodb.net/?appName=Cluster0", "")
     REDIRECT_BLOGGER_URL = os.environ.get("REDIRECT_BLOGGER_URL", "")
     BLOGGER_PAGE_URL = os.environ.get("BLOGGER_PAGE_URL", "")
     
@@ -31,4 +31,4 @@ class Config:
     else: FORCE_SUB_CHANNEL = 0
         
     # Yeh bot ka username store karega (code isse automatic set karega)
-    BOT_USERNAME = ""
+    BOT_USERNAME = "@bu_file_bot"
